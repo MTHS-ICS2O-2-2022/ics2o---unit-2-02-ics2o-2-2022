1886a0a8342cf4176bb30cb87e8fdf1a86d46c85
ics2o-2-2022 <124388837+ics2o-2-2022@users.noreply.github.com>
2023-03-09 17:44:31 +0000

    basic math

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function doMathClicked() {
+  document.getElementById('add-math').innerHTML = "<p>6 + 5 = " + (6+5) + "</p>"
+  document.getElementById("subtract-math").innerHTML = "<p>7 - 3 =  " + (7 - 3) + "</p>"
+  document.getElementById("multiply-math").innerHTML = "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>"
+  document.getElementById("divide-math").innerHTML = "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
+document.getElementById("exponent-math").innerHTML = "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+}
