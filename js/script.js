92034dfadd2b5bbfb0d3774e33664954c969a2e6
ics2o-2-2022 <124388837+ics2o-2-2022@users.noreply.github.com>
2023-03-09 17:53:38 +0000

    area and perimeter

--- js/script.js
+++ js/script.js
@@ -5,9 +5,21 @@
 // This file contains the JS functions for index.html
 
 function doMathClicked() {
-  document.getElementById('add-math').innerHTML = "<p>6 + 5 = " + (6+5) + "</p>"
-  document.getElementById("subtract-math").innerHTML = "<p>7 - 3 =  " + (7 - 3) + "</p>"
-  document.getElementById("multiply-math").innerHTML = "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>"
-  document.getElementById("divide-math").innerHTML = "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
-document.getElementById("exponent-math").innerHTML = "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+  document.getElementById("add-math").innerHTML =
+    "<p>6 + 5 = " + (6 + 5) + "</p>"
+  document.getElementById("subtract-math").innerHTML =
+    "<p>7 - 3 =  " + (7 - 3) + "</p>"
+  document.getElementById("multiply-math").innerHTML =
+    "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>"
+  document.getElementById("divide-math").innerHTML =
+    "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
+  document.getElementById("exponent-math").innerHTML =
+    "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+}
+
+function calculateClicked() {
+  document.getElementById("area-math").innerHTML =
+    "<p>The area is: " + 3 * 5 + " cm².</p>"
+  document.getElementById("perimeter-math").innerHTML =
+    "<p>The perimeter is: " + 2 * (3 + 5) + " cm.</p>"
 }
